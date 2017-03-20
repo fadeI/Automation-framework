952512e96576af9d19873ac26c91b9abcaefe8ac
fadeI <fadeisma@outlook.com>
2017-03-20 23:35:21 +0200

    adding stackBrowser run

---
+++ AutomationFramework/AutomationFramework/Example.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AutomationFramework
+{
+    [TestFixture("single", "chrome")]
+    class Example : BaseBrowserNunit
+    {
+
+        public Example(string profile, string environment) : base(profile, environment) { }
+
+        [Test]
+        public void excute()
+        {
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("BrowserStack");
+            query.Submit();
+            System.Threading.Thread.Sleep(5000);
+            Assert.AreEqual("BrowserStack - Google Search", driver.Title);
+
+        }
+    }
+}
