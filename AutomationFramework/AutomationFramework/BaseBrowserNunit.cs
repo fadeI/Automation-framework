45291c46e3fe56b0f680b774f36670f29f461be6
fadeI <fadeisma@outlook.com>
2017-03-20 21:57:11 +0200

    update StackBrowser Base

---
+++ AutomationFramework/AutomationFramework/BaseBrowserNunit.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace AutomationFramework
+{
+
+    [TestFixture]
+    class BaseBrowserNunit
+    {
+
+        protected string profile;
+        protected string environment;
+
+        [SetUp]
+        public void InitBrowserStack()
+        {
+            NameValueCollection caps = ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
+            NameValueCollection settings = ConfigurationManager.GetSection("environments/" + environment) as NameValueCollection;
+
+            
+        }
+
+    }
+}
